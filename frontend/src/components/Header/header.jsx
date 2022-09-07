090197af1979ef459f7ae1f8f2b12687c78ab2c9
Vinicius 'Davina' Ferreira <63323842+davinnye@users.noreply.github.com>
2022-09-06 21:52:45 -0300

    Other Components

---
+++ frontend/src/components/Header/header.jsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/header-icon.svg'
+import './styles.css'
+
+function Header() {
+
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta" />
+            <h1>DSMeta</h1>
+            <p>
+              Desenvolvido por 
+              <a href="https://github.com/davinnye"> @Davinnye</a>
+            </p>
+        </div>
+        </header>
+    )
+  }
+  
+  export default Header
